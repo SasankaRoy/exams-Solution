15e8af2f1def8afe45fea22a21c133f28167a4ef
Sasanka Roy <sasankaroy033@gmail.com>
2024-05-31 14:07:44 +0530

    contact us page change

--- components/Category/ContactForm/ContactDetails.tsx
+++ components/Category/ContactForm/ContactDetails.tsx
@@ -7,15 +7,11 @@ import Link from "next/link";
 export const ContactDetails = () => {
   return (
     <div className="w-[50%] border-l-2 border-[#FFF]/50 py-4">
-      <div className="w-[80%] mx-auto flex flex-col justify-start items-start gap-3">
-        <div>
+      <div className="w-[80%] mx-auto flex flex-col justify-start items-start gap-5">
+        <div className="contactShadow w-[75%] py-3 px-4 rounded-lg">
           <h2 className="text-[#FFF] font-[Poppins] font-[600] text-xl">
             Call Us
           </h2>
-          <p className="text-[#FFF] font-[Poppins] font-[300] text-sm my-3">
-            Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi,
-            pretium ut lacinia in, elementum id enim. Done
-          </p>
 
           <Link
             href="tel:+1 (289) 352-1293"
@@ -26,36 +22,30 @@ export const ContactDetails = () => {
           </Link>
         </div>
 
-        <div>
+        <div className="contactShadow w-[75%] py-3 px-4 rounded-lg">
           <h2 className="text-[#FFF] font-[Poppins] font-[600] text-xl">
             Email
           </h2>
-          <p className="text-[#FFF] font-[Poppins] font-[300] text-sm my-3">
-            Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi,
-            pretium ut lacinia in, elementum id enim. Done
-          </p>
+          
 
           <Link
             href="mailto:support@examssolutions.ca"
-            className="flex justify-start items-center text-[#FFF] gap-3 font-[500] font-[Poppins] text-base"
+            className="flex justify-start items-center text-[#FFF] gap-3 font-[500] font-[Poppins] text-base my-4"
           >
             <EmailIcon />
             <span>support@examssolutions.ca</span>
           </Link>
         </div>
 
-        <div>
+        <div className="contactShadow w-[75%] py-3 px-4 rounded-lg">
           <h2 className="text-[#FFF] font-[Poppins] font-[600] text-xl">
             Visit Us
           </h2>
-          <p className="text-[#FFF] font-[Poppins] font-[300] text-sm my-3">
-            Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi,
-            pretium ut lacinia in, elementum id enim. Done
-          </p>
+          
 
           <Link
             href="/"
-            className="flex justify-start items-center text-[#FFF] gap-3 font-[500] font-[Poppins] text-base"
+            className="flex justify-start items-center text-[#FFF] gap-3 font-[500] font-[Poppins] text-base my-4"
           >
             <LocationOnIcon />
             <span>Mississauga, ON, Canada</span>
