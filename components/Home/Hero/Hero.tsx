a75726cc0e955f8f5c8f54366ac5c74f882ca01c
Sasanka Roy <sasankaroy033@gmail.com>
2024-06-04 11:23:35 +0530

    started the hero section animation and added the vedio

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const Hero = () => {
   return (
     <div className="h-[100vh] relative bg-[#000000]/90 overflow-hidden w-full">
-      <Image fill src="/heroBg.png" alt="Hero-bg" />
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="absolute w-full h-full top-0 left-0"
+      >
+        <Image fill src="/heroBg.png" alt="Hero-bg" />
+      </motion.div>
       <div className=" absolute w-[20%] h-[45%] -left-44 circelBgColor top-5 rounded-full" />
       <div className=" absolute w-[20%] h-[45%] -right-44 circelBgColor bottom-3 rounded-full" />
 
       <div className="w-full absolute h-full flex justify-center items-center top-0 left-0">
         <div className="w-[90%] h-full flex justify-center items-center mx-auto gap-3">
           <div className="w-[50%]">
-            <h1 className="text-[#FFF] text-5xl font-[500] font-[Bakbak One] lineHeight">
+            <motion.h1 initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:.4,delay:.2}} className="text-[#FFF] text-5xl font-[500] font-[Bakbak One] lineHeight">
               Simplify your exam preparation process with
-            </h1>
+            </motion.h1>
             <h2 className="text-5xl font-[500] font-[Bakbak One] textBgColor my-3">
               ExamsSolutions.ca.
             </h2>
@@ -27,9 +35,14 @@ export const Hero = () => {
             </button>
           </div>
           <div className="w-[50%] h-full flex justify-center items-center">
-            <div className="w-[90%]  h-[50%] rounded-2xl relative videoWrapper">
-              <div className="bg-white w-full h-full rounded-2xl">
-                <video></video>
+            <div className="w-[90%]  h-[45%] rounded-2xl relative videoWrapper">
+              <div className="bg-white w-full h-full rounded-2xl overflow-hidden">
+                <video
+                  src="/Examsolution.mp4"
+                  autoPlay
+                  loop
+                  className="w-full h-full object-fill "
+                />
               </div>
             </div>
           </div>
